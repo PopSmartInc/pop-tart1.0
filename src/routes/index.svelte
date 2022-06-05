24b189dd91ce4dc5c886cca6ff9c00d6cb031f13
Derek Mason <3derekmason@gmail.com>
2022-06-05 08:02:28 -0600

    background mesh and shadows added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,17 +12,36 @@
 	});
 </script>
 
-<SC.Canvas antialias background={new THREE.Color(0x004d40)}>
+<SC.Canvas
+	antialias
+	background={new THREE.Color(0x00bfa5)}
+	shadows
+	fog={new THREE.FogExp2(0x00bfa5, 0.1)}
+>
 	<SC.Mesh
 		geometry={new THREE.BoxGeometry()}
-		material={new THREE.MeshStandardMaterial({ color: 0xff3e00 })}
+		material={new THREE.MeshStandardMaterial({ color: 0xaa00ff })}
 		scale={[width, height, depth]}
 		rotation={[0, spin, 0]}
+		castShadow
 	/>
 	<SC.PerspectiveCamera position={[1, 1, 3]} />
-	<SC.OrbitControls enableZoom={false} />
+	<SC.OrbitControls enableZoom={false} maxPolarAngle={Math.PI * 0.51} />
 	<SC.AmbientLight intensity={0.6} />
-	<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} />
+	<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} shadow={{ mapSize: [2048, 2048] }} />
+	<SC.Group position={[0, -height / 2, 0]}>
+		<SC.Mesh
+			geometry={new THREE.PlaneGeometry(50, 50)}
+			material={new THREE.MeshStandardMaterial({ color: 0x1de9b6 })}
+			rotation={[-Math.PI / 2, 0, 0]}
+			receiveShadow
+		/>
+
+		<SC.Primitive
+			object={new THREE.GridHelper(50, 50, 0x444444, 0x555555)}
+			position={[0, 0.001, 0]}
+		/>
+	</SC.Group>
 </SC.Canvas>
 
 <div class="controls">
